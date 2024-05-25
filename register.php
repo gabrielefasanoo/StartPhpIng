724851dc48dd44f1e38785410ad2a26b63b757de
Domenico Francavilla <comm.francavilla.domenico@gmail.com>
2024-05-25 14:24:09 +0200

    modifica sessione

--- register.php
+++ register.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         /*$_SESSION['user_id'] = $conn->insert_id;
         $_SESSION['user_name'] = $name;*/
         echo "Registration successful";
-        header("Location: index.html");
+        header("Location: index.php");
         exit();
     } else {
         echo "Error: unable to insert data";
