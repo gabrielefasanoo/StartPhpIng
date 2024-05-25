334bb6eaa204ff5a0025f3c017624ac2352be0f7
gabrielefasanoo <gabrielefasano13@gmail.com>
2024-05-25 15:01:48 +0200

    modifica sito con ricerca

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -14,9 +15,11 @@ session_start();
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
 </head>
+
 <body>
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -30,7 +33,7 @@ session_start();
                     <li class="nav-item"><a class="nav-link" href="#!">Pattern</a></li>
                 </ul>
                 <div>
-                    <?php if (isset($_SESSION['username'])): ?>
+                    <?php if (isset($_SESSION['username'])) : ?>
                         <!-- Show username and logout button if user is logged in -->
                         <div class="btn-group">
                             <button type="button" class="btn btn-outline-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
@@ -40,10 +43,11 @@ session_start();
                                 <li><a class="dropdown-item" href="logout.php">Logout</a></li>
                             </ul>
                         </div>
-                    <?php else: ?>
+                    <?php else : ?>
                         <!-- Show login and register buttons if user is not logged in -->
                         <button class="btn btn-outline-dark mx-2" type="button" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
                         <button class="btn btn-outline-dark" type="button" data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
+
                     <?php endif; ?>
                 </div>
 
@@ -132,10 +136,12 @@ session_start();
     </nav>
     <!-- Header-->
     <header class="bg-dark py-5">
-        <div class="container px-4 px-lg-5 my-5">
+        <div class="container px-2 px-lg-5 my-5">
             <div class="text-center text-white">
-                <h1 class="display-4 fw-bolder"></h1>
-                <p class="lead fw-normal text-white-50 mb-0"></p>
+                <div class="input-group">
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                    <button type="button" class="btn btn-outline-primary" data-mdb-ripple-init>search</button>
+                </div>
             </div>
         </div>
     </header>
@@ -250,29 +256,32 @@ session_start();
                         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                         <!-- Product details-->
                         <div class="card-body p-4"></div>
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder"></h5>
-                                <!-- Product price-->
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent"></div>
-                            <div class="text-center"></div>
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5 class="fw-bolder"></h5>
+                            <!-- Product price-->
                         </div>
                     </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent"></div>
+                    <div class="text-center"></div>
                 </div>
-            
             </div>
+        </div>
+
+        </div>
         </div>
     </section>
     <!-- Footer-->
     <footer class="py-5 bg-dark">
-        <div class="container"><p class="m-0 text-center text-white"></p></div>
+        <div class="container">
+            <p class="m-0 text-center text-white"></p>
+        </div>
     </footer>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
     <script src="js/scripts.js"></script>
 </body>
+
 </html>
