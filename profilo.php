db06f3326449f1dcbad3aa90d564ca204b89cbec
Domenico Francavilla <comm.francavilla.domenico@gmail.com>
2024-06-05 19:48:46 +0200

    rimozione GDPR

--- profilo.php
+++ profilo.php
@@ -54,7 +54,6 @@ if ($result->num_rows > 0) {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">PKB table</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">GDPR info</a></li>
                     <li class="nav-item"><a class="nav-link" href="#!">Area Feedback</a></li>
                 </ul>
                 <div>
