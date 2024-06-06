7cdbfa674f93c92ed50d85c3d69cc1eabb754cd3
Domenico Francavilla <comm.francavilla.domenico@gmail.com>
2024-06-06 16:04:08 +0200

    correzione link

--- index.php
+++ index.php
@@ -27,15 +27,7 @@ session_start();
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-<<<<<<< HEAD
-
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="pkb.php">PKB table</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">GDPR info</a></li>
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">PKB table</a></li>
-
-=======
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="pkb.php">PKB table</a></li>
->>>>>>> db06f3326449f1dcbad3aa90d564ca204b89cbec
                     <li class="nav-item"><a class="nav-link" href="#!">Area Feedback</a></li>
                 </ul>
                 <div>
--- pkb.php
+++ pkb.php
@@ -28,8 +28,7 @@ if (!isset($_SESSION['email'])) { // Verifica l'email anziché l'username
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">PKB table</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">GDPR info</a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="pkb.php">PKB table</a></li>
                 <li class="nav-item"><a class="nav-link" href="#!">Area Feedback</a></li>
             </ul>
             <div>
